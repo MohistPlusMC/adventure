c9cda192b4c2b99527977c5b08bc2300f535371c
ZX夏夜之风 <snwcreations@qq.com>
2025-08-02 19:14:03 +0800

    fix: do not use class loader of service type
    The built-in Adventure services should not to be duplicated in running environment, so it is safe to use thread context class loader.

--- api/src/main/java/net/kyori/adventure/util/Services0.java
+++ api/src/main/java/net/kyori/adventure/util/Services0.java
@@ -30,6 +30,6 @@ final class Services0 {
   }
 
   static <S> ServiceLoader<S> loader(final Class<S> type) {
-    return ServiceLoader.load(type, type.getClassLoader());
+    return ServiceLoader.load(type/*, type.getClassLoader()*/); // Mohist+ - Do not use class loader of service type
   }
 }
